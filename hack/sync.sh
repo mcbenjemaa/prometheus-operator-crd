72e696ee308e04469c3e37b7902dbd88fe21f996
Mohamed Chiheb <mohamed@giantswarm.io>
2022-09-27 09:28:56 +0100

    Solve Review notes

--- hack/sync.sh
+++ hack/sync.sh
@@ -2,13 +2,14 @@
 #
 # This script sync CRDs from upstream prometheus-operator helm chart
 
-version=${VERSION:-master}
+version=${VERSION:-main}
 tmp="/tmp/helm-charts-$version"
 upstream=https://github.com/prometheus-community/helm-charts
 
 
 function main() {
 git clone --depth 1  --branch "kube-prometheus-stack-${version}" "$upstream" "$tmp"
+rm -rf ./helm/prometheus-operator-crd/templates/
 cp -rf "$tmp/charts/kube-prometheus-stack/crds/" ./helm/prometheus-operator-crd/templates/
 }
 
